11ce076ae3a9366a5e78138d75995145fcb3efa6
Somfer <furmanets_s@mail.ru>
2022-11-22 09:55:08 +0300

    fix lab2 finish laba2

--- laba 2/main.py
+++ laba 2/main.py
@@ -2,8 +2,6 @@ from os import system
 import matplotlib.pyplot as plt
 import math
 
-fig, ax = plt.subplots()
-
 # Начальные настройки
 h = 0
 t = []
@@ -13,26 +11,28 @@ xe = []
 ye = []
 
 
-def Init(a, b):  # ИнициализацияЫ
+def Init(p, p1):  # Инициализация
+    a = 2
+    b = 1
     t.clear()
     x.clear()
     y.clear()
     xe.clear()
     ye.clear()
 
-    D = Decisions(a)
+    D = Decisions(p)
     De = Euler(a, b)
-    print(D)
-    print(De)
+    #print(D)
+    #print(De)
 
     i = 0
-    t.append(a)
+    t.append(p)
     x.append(D[0])
     y.append(D[1])
     xe.append(De[0])
     ye.append(De[1])
 
-    while i < b:
+    while i < p1:
         i += h
         D = Decisions(i)
         De = Euler(xe[len(xe)-1], ye[len(ye)-1])
@@ -61,14 +61,19 @@ def Euler(x, y):  # Решения Эйлера
 
 
 def Scaling():  # Графики
+    fig.set_figwidth(8)
+    fig.set_figheight(8)
     plt.grid()
-    ax.plot(x, y, c='b')
-    ax.plot(xe, ye, c='r')
+    plt.xlabel("x")
+    plt.ylabel("y")
+    ax.scatter(x, y, c='red', label="В явном виде",s=1)  # Красный в явном виде
+    ax.scatter(xe, ye, c='blue', label="Метод Эйлера",s=0.5)  # Синий Эйлер
+    plt.legend(fontsize=14)
+    plt.tight_layout()
     plt.show()
 
 
 def Table():  # Таблица
-    fig, ax = plt.subplots()
     fig.set_figwidth(10)
     fig.set_figheight(10)
     fig.patch.set_visible(False)
@@ -76,12 +81,15 @@ def Table():  # Таблица
     ax.axis('off')
     col_labels = ['T', 'X', 'Y', 'X_E', 'Y_E']
     table_vals = []
-    for i in range(0, 50):
+    size=0
+    if len(x)>50: size=51
+    else: size=len(x)
+    for i in range(0, size):
         tmp = [t[i], x[i], y[i], xe[i], ye[i]]
         table_vals.append(tmp)
 
     the_table = ax.table(cellText=table_vals,
-                         colWidths=[0.3] * len(col_labels),
+                         colWidths=[0.2] * len(col_labels),
                          rowLoc='center',
                          colLabels=col_labels,
                          cellLoc="center",
@@ -90,6 +98,16 @@ def Table():  # Таблица
     the_table.set_fontsize(12)
     the_table.scale(1, 1)
     plt.show()
+
+def СheckSpecialpoint(l1,l2,b1=0,b2=0):
+    print('Особая точка (0;0) - ', end='')
+    if l1<0 and l2<0 and b1==0 and b2==0: print('Устойчивый узел')
+    elif l1>0 and l2>0 and b1==0 and b2==0: print('Неустойчивый узел')
+    elif l1>0 and l2<0 and b1==0 and b2==0 or l1<0 and l2>0 and b1==0 and b2==0: print('Седло')
+    elif l1<0 and l2<0 and b1!=0 and b2!=0: print('Устойчивый фокус')
+    elif l1>0 and l2>0 and b1!=0 and b2!=0: print('Неустойчивый фокус')
+    else: print('Неизвестно')
+
 
 
 def Distance(x, y, x1, y1):  # Расстояние между двумя точками
@@ -106,18 +124,22 @@ def MaxChange():  # Максимальное отклонение
 def menu():  # Меню выбора
     print('1. Меняем начальные настройки')
     print('2. Графики')
-    print('3. Таблица')
-    print('4. Максимальное расхождение')
-    print('5. Выход')
+    print('3. Таблица значений(первые 50)')
+    print('4. Вид особой точки')
+    print('5. Максимальное расхождение')
+    print('6. Выход')
 
 
 exit = True
 while (exit):
+    fig, ax = plt.subplots()
     print('Для продожений нажмите Enter')
     input()
+    h=0.001
+    Init(0,2)
     system("cls")
     menu()
-    print('Выберете действие:', end='')
+    print('Выберете действие: ', end='')
     select = int(input())
     if select == 1:
         print("Введите диапазон значений: ", end='')
@@ -129,7 +151,7 @@ while (exit):
         else:
             print('Неверное количество значений')
             continue
-        print('Введите шаг:', end='')
+        print('Введите шаг: ', end='')
         str = input()
         harr = str.split(' ')
         h = float(harr[0])
@@ -140,10 +162,10 @@ while (exit):
     elif select == 3:
         Table()
     elif select == 4:
-        print(MaxChange())
-    # elif select == 5:
-       # Stability()
+       СheckSpecialpoint(-1,3)
     elif select == 5:
+        print(MaxChange())
+    elif select == 6:
         exit = False
     else:
         print("Некорректный пункт меню")
